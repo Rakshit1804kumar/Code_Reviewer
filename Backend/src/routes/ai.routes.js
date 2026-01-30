fc033eb241be5975cddb3726a38b31d5bca12598
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-01-30 22:57:54 +0530

    Update ai.routes.js

--- Backend/src/routes/ai.routes.js
+++ Backend/src/routes/ai.routes.js
@@ -1,10 +1,6 @@
 const express = require('express');
-//const aiController = require("../controllers/ai.controller")
-exports.getReview = async (req, res) => {
-  return res.json({
-    review: "Controller working perfectly ✅"
-  });
-};
+const aiController = require("../controllers/ai.controller")
+ 
 const router = express.Router();
 
 router.post("/get-review",aiController.getReview)
