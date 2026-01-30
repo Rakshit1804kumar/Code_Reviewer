0b1d3a10b7e5f486964130a7cb806295155e1838
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-01-30 23:40:18 +0530

    Update ai.services.js

--- Backend/src/services/ai.services.js
+++ Backend/src/services/ai.services.js
@@ -2,7 +2,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 const model = genAI.getGenerativeModel({
-    model: "models/gemini-1.5-flash",
+    model: "models/gemini-1.5-flash-latest",
     systemInstruction: `
                 Here’s a solid system instruction for your AI code reviewer:
 
